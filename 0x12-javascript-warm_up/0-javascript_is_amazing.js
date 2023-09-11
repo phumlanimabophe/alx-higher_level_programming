3d5736057da7a2e2172ed5058af3a7667d886923
Phumlani Mabophe <93977501+phumlanimabophe@users.noreply.github.com>
2023-09-11 20:35:35 +0000

    first

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+// Print string “Javascript is amazing”
+
+const aVar = 'Javascript is amazing';
+console.log(aVar);
---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+//  new function incr that increments the integer value.
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
