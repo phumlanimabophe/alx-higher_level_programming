a5baacfc22de9374c7743b0f80a278c80042d97e
Phumlani Mabophe <93977501+phumlanimabophe@users.noreply.github.com>
2023-09-12 22:25:34 +0000

    final

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const fs = require('fs');
-const a = fs.readFileSync(process.argv[2], 'utf8');
-const b = fs.readFileSync(process.argv[3], 'utf8');
+const data = require('fs');
+const a = data.readFileSync(process.argv[2], 'utf8');
+const b = data.readFileSync(process.argv[3], 'utf8');
 fs.writeFileSync(process.argv[4], a + b);
