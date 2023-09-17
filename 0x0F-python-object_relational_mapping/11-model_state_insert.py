260c15eecae615bfc826f46ac34fe16dde05871b
Phumlani Mabophe <93977501+phumlanimabophe@users.noreply.github.com>
2023-09-17 20:37:16 +0000

    start

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,21 +1,44 @@
 #!/usr/bin/python3
-# Adds the State object "Louisiana" to the database hbtn_0e_6_usa.
-# Usage: ./11-model_state_insert.py <mysql username> /
-#                                   <mysql password> /
-#                                   <database name>
+
+"""
+A script that adds the State object 'Louisiana' to the database hbtn_0e_6_usa.
+Usage: ./model_state_insert.py <mysql username> <mysql password> <database name>
+"""
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from model_state import Base, State
+
+def insert_state(username, password, database_name):
+    try:
+        engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                               .format(username, password, database_name),
+                               pool_pre_ping=True)
+        Session = sessionmaker(bind=engine)
+
+        Base.metadata.create_all(engine)
+
+        # create a session
+        session = Session()
+
+        # create the object and add it
+        new_state = State(name='Louisiana')
+        session.add(new_state)
+        session.commit()
+
+        # print state.id
+        state_add = session.query(State).filter(State.name == 'Louisiana').one()
+        print(state_add.id)
+
+        session.close()
+    except Exception as e:
+        print("Error:", str(e))
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    louisiana = State(name="Louisiana")
-    session.add(louisiana)
-    session.commit()
-    print(louisiana.id)
+    if len(sys.argv) != 4:
+        print("Usage: ./model_state_insert.py <mysql username> <mysql password> <database name>")
+        sys.exit(1)
+    
+    username, password, database_name = sys.argv[1], sys.argv[2], sys.argv[3]
+    insert_state(username, password, database_name)
--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,22 +1,45 @@
 #!/usr/bin/python3
-# Deletes all State objects with a name containing
-# the letter a from the database hbtn_0e_6_usa.
-# Usage: ./13-model_state_delete_a.py <mysql username> /
-#                                     <mysql password> /
-#                                     <database name>
+
+"""
+A script that deletes all State objects with a name containing the letter 'a'
+from the database hbtn_0e_6_usa.
+Usage: ./model_state_delete_a.py <mysql username> <mysql password> <database name>
+"""
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from model_state import Base, State
+
+def delete_states_with_a(username, password, database_name):
+    try:
+        engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                               .format(username, password, database_name),
+                               pool_pre_ping=True)
+        Session = sessionmaker(bind=engine)
+
+        Base.metadata.create_all(engine)
+
+        # create a session
+        session = Session()
+
+        # extract states with 'a' in their names
+        states = session.query(State).filter(State.name.ilike('%a%')).all()
+
+        # delete states
+        for state in states:
+            session.delete(state)
+
+        session.commit()
+
+        session.close()
+    except Exception as e:
+        print("Error:", str(e))
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    for state in session.query(State):
-        if "a" in state.name:
-            session.delete(state)
-    session.commit()
+    if len(sys.argv) != 4:
+        print("Usage: ./model_state_delete_a.py <mysql username> <mysql password> <database name>")
+        sys.exit(1)
+    
+    username, password, database_name = sys.argv[1], sys.argv[2], sys.argv[3]
+    delete_states_with_a(username, password, database_name)
