477d42da97192cd6ae7c965053cb7c48443feb94
Phumlani Mabophe <93977501+phumlanimabophe@users.noreply.github.com>
2023-09-17 20:05:23 +0000

    start

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,13 +1,30 @@
 #!/usr/bin/python3
-# Lists all states with a name starting with N from the database hbtn_0e_0_usa.
-# Usage: ./1-filter_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
+
+"""
+A script that lists all states with names starting with 'N' from the database hbtn_0e_0_usa.
+Usage: ./list_states_with_N.py <mysql username> <mysql password> <database name>
+"""
+
 import sys
 import MySQLdb
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states` ORDER BY `id`")
-    [print(state) for state in c.fetchall() if state[1][0] == "N"]
+def list_states_with_N(username, password, database_name):
+    try:
+        db = MySQLdb.connect(user=username, passwd=password, db=database_name, host='localhost', port=3306)
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+        data = cursor.fetchall()
+        for row in data:
+            print(row)
+        cursor.close()
+        db.close()
+    except MySQLdb.Error as e:
+        print("Error connecting to the database:", str(e))
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print("Usage: ./list_states_with_N.py <mysql username> <mysql password> <database name>")
+        sys.exit(1)
+    
+    username, password, database_name = sys.argv[1], sys.argv[2], sys.argv[3]
+    list_states_with_N(username, password, database_name)
