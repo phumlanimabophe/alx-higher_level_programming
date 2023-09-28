d3c71b0dadd26a7a071f5284f163b62d049e9229
Phumlani Mabophe <93977501+phumlanimabophe@users.noreply.github.com>
2023-09-28 21:44:44 +0000

    final

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-curl -sX POST $1 -H "Content-Type: application/json" -d @$2 -L
+# Sends a JSON POST request to a given URL with a given JSON file.
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
